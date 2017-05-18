a90723e9a62db6b6c455b2d1f9f53656ae26abea
Chris Grieger <chris@scolp.de>
2017-05-18 07:50:35 +0200

    feat: pin text color contrast (fixes #13)

--- src/app/pin/pin.component.ts
+++ src/app/pin/pin.component.ts
@@ -15,6 +15,10 @@ export class PinComponent implements OnInit {
   ngOnInit() {
   }
 
+  useContrast(color: string): boolean {
+    return parseInt((color || '#fff').substring(1), 16) <= 0xffffff / 2;
+  }
+
   pinResCode(pin: Pin): string {
     if (pin.resistor === 'NONE') { return ''; };
     if (pin.resistor === 'PULL_UP') { return 'U'; };
